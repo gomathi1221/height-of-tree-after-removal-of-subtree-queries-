b1dfebc1e0ed59a6c05a886716f82c3a2af97c48
gomathi1221 <166113706+gomathi1221@users.noreply.github.com>
2024-10-26 22:56:25 +0530

    Add files via upload

---
+++ height of binary tree after subtree removal queries.java
@@ -0,0 +1,41 @@
+class Solution {
+    int[] po = new int[100001];
+    int[] po_ = new int[100001];
+    int maxH = 0;
+
+    private void preOrder(TreeNode root, int height) {
+        // edge
+        if (root == null) {
+            return;
+        }
+
+        po[root.val] = maxH;
+        maxH = Math.max(maxH, height);
+        preOrder(root.left, height + 1);
+        preOrder(root.right, height + 1);
+    }
+
+    private void postOrder(TreeNode root, int height) {
+        // edge
+        if (root == null) {
+            return;
+        }
+
+        po_[root.val] = maxH;
+        maxH = Math.max(maxH, height);
+        postOrder(root.right, height + 1);
+        postOrder(root.left, height + 1);
+    }
+
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        preOrder(root, 0);
+        maxH = 0;
+        postOrder(root, 0);
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = Math.max(po[queries[i]], po_[queries[i]]);
+        }
+        return res;
+    }
+}
